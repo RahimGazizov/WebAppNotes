ee58556a4cb81e656fb6481761a2c5756db4f786
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-24 15:24:26 +0600

    Добавлена миграция NewColumnUser (новый столбец в таблице User)

---
+++ Migrations/20260123064635_NewColumnUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewColumnUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResetPassword",
+                table: "Users",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetPassword",
+                table: "Users");
+        }
+    }
+}
