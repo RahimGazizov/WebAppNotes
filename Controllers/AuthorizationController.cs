588d685d3bcb0cc2d27d6c451f50bbecca3daf10
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-24 15:12:04 +0600

    Вынес хэширование паролей из AuthorizationController в отдельный класс HashCode

--- Controllers/AuthorizationController.cs
+++ Controllers/AuthorizationController.cs
@@ -6,14 +6,17 @@ using System.Security.Cryptography;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
+using NotesApp.SendEmail;
 namespace NotesApp.Controllers
 {
     public class AuthorizationController : Controller
     {
         private readonly AppDbContext _context;
-        public AuthorizationController(AppDbContext context)
+        private readonly HashPassword.HashCode _hash;
+        public AuthorizationController(AppDbContext context, HashPassword.HashCode hash)
         {
             _context = context;
+            _hash = hash;
         }
         public IActionResult Index()
         {
@@ -29,13 +32,13 @@ namespace NotesApp.Controllers
                 TempData["ErrorMessage"] = "Такого пользователя не существует";
                 return View();
             }
-            bool isVerification = VereficationPassword(authorization.Password, user.Password);
+            bool isVerification = _hash.VereficationPassword(authorization.Password, user.Password);
             if (!isVerification)
             {
                 TempData["ErrorMessage"] = "Неверный логин или пароль";
                 return View();
             }
-            
+
             var claim = new List<Claim>()
             {
                 new Claim(ClaimTypes.NameIdentifier, user?.Id.ToString() ?? "0")
@@ -46,20 +49,7 @@ namespace NotesApp.Controllers
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
             return RedirectToAction("Index", "Notes");
         }
-        private bool VereficationPassword(string password, string hashStand)
-        {
-            byte[] hashCode = Convert.FromBase64String(hashStand);
-            byte[] salt = new byte[16];
-            Array.Copy(hashCode, 0, salt, 0, 16);
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
-            var hash = pbkdf2.GetBytes(32);
-
-            for (int i = 0; i < 32; i++)
-            {
-                if (hashCode[i + 16] != hash[i])
-                    return false;
-            }
-            return true;
-        }
+        
+      
     }
 }
