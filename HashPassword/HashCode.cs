8ac8c07ee6eb1f31392db504ce8ef5bb26f14512
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-24 15:22:39 +0600

    добавл новую папку для хэшпароля

---
+++ HashPassword/HashCode.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+
+namespace NotesApp.HashPassword
+{
+    public class HashCode
+    {
+        public string HashPassword(string password)
+        {
+            byte[] salt = new byte[16];
+            using (var rgb = RandomNumberGenerator.Create())
+            {
+                rgb.GetBytes(salt);
+            }
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+            byte[] hashCode = new byte[48];
+            Array.Copy(salt, 0, hashCode, 0, 16);
+            Array.Copy(hash, 0, hashCode, 16, hash.Length);
+            return Convert.ToBase64String(hashCode);
+        }
+        public bool VereficationPassword(string password, string hashStand)
+        {
+            byte[] hashCode = Convert.FromBase64String(hashStand);
+            byte[] salt = new byte[16];
+            Array.Copy(hashCode, 0, salt, 0, 16);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000, HashAlgorithmName.SHA256);
+            var hash = pbkdf2.GetBytes(32);
+
+            for (int i = 0; i < 32; i++)
+            {
+                if (hashCode[i + 16] != hash[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
