e23212772e85dcf15187f36ec9e1c57485bfb27c
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-25 14:24:41 +0600

    новая модель для хранения паролей

---
+++ Models/Password.cs
@@ -0,0 +1,9 @@
+﻿namespace NotesApp.Models
+{
+    public class Password
+    {
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
