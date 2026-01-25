f88992f733be1174ae17bbfb3f6289a52bcc9fdf
Rah1m03 <rahimgazizov2003@gmail.com>
2026-01-25 14:25:10 +0600

    модель для хранения user и password

---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NotesApp.Models;
+
+namespace NotesApp.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public User User { get; set; }
+        public Password Password { get; set; }
+    }
+}
